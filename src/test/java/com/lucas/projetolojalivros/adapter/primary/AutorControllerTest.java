68e1af8779a601df5ea0fdd481ca8d4e0891a220
Lucas Lopes <lucas_lsilva@terceiros.sicredi.com.br>
2023-08-12 18:10:47 -0300

    Feat/implementa rest docs

--- src/test/java/com/lucas/projetolojalivros/adapter/primary/AutorControllerTest.java
+++ src/test/java/com/lucas/projetolojalivros/adapter/primary/AutorControllerTest.java
@@ -1,6 +1,15 @@
 package com.lucas.projetolojalivros.adapter.primary;
 
 import static org.hamcrest.Matchers.is;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -10,10 +19,15 @@ import com.lucas.projetolojalivros.helper.annotations.autor.RemoverAutoresSql;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 class AutorControllerTest extends BaseIntegrationTest {
 
     @Test
@@ -27,6 +41,21 @@ class AutorControllerTest extends BaseIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(toJson(autorAtual)))
             .andExpect(status().isCreated())
-            .andExpect(header().string(HttpHeaders.LOCATION, is("/v1/autores/1")));
+            .andExpect(header().string(HttpHeaders.LOCATION, is("/v1/autores/1")))
+            .andDo(document("v1/autores",
+                preprocessRequest(prettyPrint()),
+                preprocessResponse(prettyPrint()),
+                requestFields(
+                    fieldWithPath("nome").description("Nome do autor"),
+                    fieldWithPath("email").description("Email do autor"),
+                    fieldWithPath("descricao").description("Descrição do autor")
+                ),
+                responseHeaders(
+                    headerWithName("location").description("Localização do autor criado")
+                ),
+                responseFields(
+                    fieldWithPath("id").description("Id do autor criado")
+                ))
+            );
     }
 }
